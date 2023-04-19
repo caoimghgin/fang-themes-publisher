6c13b638e3d0b35ac37ab889c2b522a31e3f4865
Kevin Muldoon <kmuldoon@mac.com>
2023-04-19 07:29:32 -0400

    Adding Optimistic Parsing Option

--- cti+/extensions/parsers/utilities/ctiPlusSchemaParser.js
+++ cti+/extensions/parsers/utilities/ctiPlusSchemaParser.js
@@ -7,10 +7,40 @@
 const _ = require('lodash');
 const consts = require('../../../constants')
 const utils = require('../../../utilities')
+const { ENV } = require('../../../../package.json')
 
 function ctiPlusSchemaParser(dictionary, keys) {
     schemaRouteParser(dictionary, keys)
     colorClassParser(dictionary, keys)
+    optimisticParser(dictionary, keys)
+
+}
+
+/**
+ * If OPTIMISTIC_PARSING is true, CTI+ will assume best taxonomy and populate.
+ */
+const optimisticParser = (dictionary, keys) => {
+    if (!ENV.OPTIMISTIC_PARSING) return
+    for (const key of keys) {
+        const token = _.get(dictionary, key)
+        token.$schema.taxonomy.domain = ENV.DOMAIN.toLowerCase()
+        let path = token.$schema.route.split(".")
+        if (token.$schema.class == 'color') {
+            if (token.$schema.route.includes(".base.") || token.$schema.route.includes(".palette.") ) {
+
+                let blk = ["base", "color", "palette"]
+                let aaa = arr = path.filter(item => !blk.includes(item))
+                console.log("aaa", aaa, path)
+
+
+                token.$schema.taxonomy.category = 'palette' 
+                token.$schema.taxonomy.state = aaa.join("-")
+                // I can populate the state by removing color, base and palette from the route. What remains is the state.
+            } else {
+                token.$schema.taxonomy.category = 'color' 
+            }
+        }
+    }
 }
 
 const schemaRouteParser = (dictionary, keys) => {
