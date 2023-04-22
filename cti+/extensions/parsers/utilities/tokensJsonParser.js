2ab6c0892040ddacc294cc77f7d21302a346389a
Kevin Muldoon <kmuldoon@mac.com>
2023-04-22 11:31:49 -0400

    Update tokensJsonParser.js

--- cti+/extensions/parsers/utilities/tokensJsonParser.js
+++ cti+/extensions/parsers/utilities/tokensJsonParser.js
@@ -2,7 +2,7 @@
  * @name tokenJsonParser
  * @type parser
  * @param  {JSON} contents The contents of the tokens.json file
- * @param  {Bool} stripW3C replace `$value` with `value` and `$description` with `comment`
+ * @param  {Bool} stripW3CPrefix replace `$value` with `value` and `$description` with `comment`
  * @return {Object} JSON parsed into Javascript object
  * @author Lukas Oppermann
  * @link https://github.com/lukasoppermann/style-dictionary-utils/blob/main/src/parser/w3c-token-json-parser.ts
@@ -10,13 +10,11 @@
  * with `comment` to make it work with style dictionary. Else, returns parsed contents
  */
 
-function tokensJsonParser(contents, stripW3C) {
-    if (stripW3C) {
-        const preparedContent = (contents || '{}').replace(/"\$?value"/g, '"value"')
-            .replace(/"\$?description"/g, '"comment"');
-        return JSON.parse(preparedContent);
-    }
-    return JSON.parse(contents);
+function tokensJsonParser(contents, stripW3CPrefix) {
+    if (!stripW3CPrefix) return JSON.parse(contents)
+    const preparedContent = (contents || '{}').replace(/"\$?value"/g, '"value"')
+        .replace(/"\$?description"/g, '"comment"');
+    return JSON.parse(preparedContent);
 }
 
 module.exports = { tokensJsonParser }
