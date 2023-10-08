c4ba7126a007648d0fabfd81610717852ba6c6d8
Kevin Muldoon <kmuldoon@mac.com>
2023-10-08 08:53:02 -0400

    Remove unused code

--- cti+/extensions/parsers/helpers/schemaParser.js
+++ cti+/extensions/parsers/helpers/schemaParser.js
@@ -7,7 +7,7 @@
  * @description inserts CTI+ schema into every node for processing/filtering
  */
 
-const { SCHEMA, ENV, COLOR, DIMENSION } = require('../../../constants')
+const { SCHEMA, COLOR, DIMENSION } = require('../../../constants')
 const utils = require('../../../utilities')
 const mapper = require('../../../mapper')
 const _ = require('lodash');
@@ -16,16 +16,6 @@ function schemaParser(dictionary, keys) {
     routeParser(dictionary, keys)
     classParser(dictionary, keys)
     mappingParser(dictionary, keys)
-    // modeParser(dictionary, keys)
-}
-
-const modeParser = (dictionary, keys) => {
-    for (const key of keys) {
-        const token = _.get(dictionary, key)
-        console.log("SHOULD BE FINE ->", token.$schema.route)
-        // utils.parseMode(token)
-        // mapper.setModeForToken(token)
-    }
 }
 
 const routeParser = (dictionary, keys) => {
@@ -49,68 +39,12 @@ const classParser = (dictionary, keys) => {
     }
 }
 
-// The keys we create are a SET, so there are no duplicates.
-// Yet, the contextual colors should have duplicates (one for 
-// light, another for dark), so we'll need to figure this out.
-
-// If I find an item using a KEY made by this, I should be able to 'filter'
-// and find multiple values if needed.
-
-const mything =  {
-    key: 'PAPERPP',
-    name: 'fang-paper-pp',
-    route: 'color.contextual.dark-mode.paper.pp',
-    mapped: true,
-    domain: 'fang',
-    brand: null,
-    subbrand: null,
-    class: 'COLOR',
-    subclass: 'PAPER',
-    mode: null,
-    taxonomy: {
-      domain: 'fang',
-      category: 'paper',
-      type: null,
-      item: null,
-      subitem: null,
-      variant: null,
-      state: 'pp',
-      context: null
-    }
-  }
-
 const mappingParser = (dictionary, keys) => {
     const map = mapper.FANG_SCHEMAS
     for (const key of keys) {
         const token = _.get(dictionary, key)
         if (!token.$schema.mapped) {
-            mapper.assignSchema(token, map)
-        }
-
-        console.log(`----------------> key:${key} mode:${token.$schema.mode} route:${token.$schema.route}`)    
-
-    }
-}
-
-const schemaMappingParser = (dictionary, keys, tokenClass, map) => {
-
-    if (map === null) {undefinedSchemaParser(dictionary, keys); return}
-
-    for (const key of keys) {
-        const token = _.get(dictionary, key)
-        if (!token.$schema.mapped) mapper.setSchemaForToken(map, token)
-    }
-
-    function undefinedSchemaParser(dictionary, keys) {
-        for (const key of keys) {
-            const token = _.get(dictionary, key)
-            if (token.$schema.map == null) {
-                token.$schema.taxonomy.domain = ENV.DOMAIN.UNDEFINED
-                token.$schema.taxonomy.context = key
-                if (token.$schema.class == COLOR.CLASS) {
-                    token.$schema.subclass = COLOR.SUBCLASS.DEFINITIVE
-                }
-            }
+            mapper.assignSchema(token, null)
         }
     }
 }
--- cti+/mapper.js
+++ cti+/mapper.js
@@ -1,117 +1,27 @@
-const { SCHEMA, COLOR, MODE } = require('./constants')
-const { isContextualColor, routeCleaner, schemaMapContructor, parseMode } = require('./utilities')
+const { routeCleaner, parseMode } = require('./utilities')
 
 const FANG_PALETTE = require("./schemas/fang/palette")
 const FANG_CONTEXTUAL = require("./schemas/fang/contextual")
 const FANG_SIZE = require("./schemas/fang/size")
 
-let x = {
-    key: 'PALETTEPRIMARY025',
-    name: 'fang-palette-primary-025',
-    domain: 'FANG',
-    class: 'COLOR',
-    subclass: 'PALETTE',
-    mode: null,
-    taxonomy: {
-      domain: 'fang',
-      category: 'palette',
-      type: null,
-      item: null,
-      variant: 'primary',
-      subitem: null,
-      state: '025',
-      context: null
-    }
-  }
+const assignSchema = (token, schemas) => {
 
-const setSchemaForToken = (schemas, token) => {
-
-    let result = null
+    if (!schemas) schemas = systemSchemas()
 
     const route = routeCleaner(token.$schema.route)
     const schema = schemas.filter(schema => !token.$schema.mapped && route.endsWith(schema.key))
 
-    // This filter will only find the first item.
-
-    if (schema.length === 0) return undefined
+    if (schema.length === 0) return
 
     if (schema.length > 1) throw new Error(`"${schema}" has more than one value for tokenAttributesForKey. 
     All taxonomy definitions need to be unique.`);
 
-    result = schema[0]
+    let result = schema[0]
     if (result) {
-
-        result.route = token.$schema.route // Add route back in
         result.mapped = true // Set mapped to true, so it is not remapped later.
-        result.mode = parseMode(token)
-        // token.$schema = result
-        // console.log(`ADD ROUTE: ${token.$schema.route}`)
-
-    }
-
-    return result
-
-}
-
-const myParseMode = (token) => {
-    if (!isContextualColor(token)) return MODE.NULL
-
-    // return ((getRandomInt(2) % 2 === 0) ? "DARK" : "LIGHT")
-    // return MODE.DARK
-
-    const path = token.$schema.route.split(".");
-    // But honestly, I should path.pop() off the key, right? Not just the end of the route
-    path.pop();
-    console.log("THE PATH", path)
-    //
-    // Something is wrong here. I wonder what th epath is???
-    //
-    const isDark = path.find(item => {
-        const pathItem = item.toUpperCase()
-        return (pathItem.startsWith(MODE.DARK) || pathItem.endsWith(MODE.DARK));
-    })
-
-    return (isDark ? "DARK" : "LIGHT")
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-      }
-}
-
-
-const setModeForToken = (token) => {
-
-    // Only contextual colors have dark/light modes. If token is not 
-    // a contextual color return null
-
-    if (!isContextualColor(token)) return null
-    // console.log(`${token.$schema.name} is a CONTEXTUAL token...`)
-
-    // Get path to the token without the end leaf node
-    const route = token.$schema.route.split("."); route.pop()
-
-    // Set the $schema.mode to dark or light mode
-    token.$schema.mode = (isDarkMode(route) ? COLOR.MODE.DARK : COLOR.MODE.LIGHT)
-    // console.log(`I have determined that the mode is "${token.$schema.mode}" given the route path ${route}`)
-
-    // Get path to the token without the end leaf node. If a color is
-    // named 'ink.dark', it is not considered dark mode. We only look at 
-    // the container directories above.
-    function getPath(token) {
-        let result = token.$schema.route.split("."); result.pop()
-        return result
-    }
-
-    // If a container of a token begins or ends with the word 'dark', 
-    // then it is dark mode.
-    function isDarkMode(route) {
-        const startsWithDark = route.find(item => {
-            return item.toUpperCase().startsWith(COLOR.MODE.DARK.toUpperCase());
-        })
-        const endsWithDark = route.find(item => {
-            return item.toUpperCase().endsWith(COLOR.MODE.DARK.toUpperCase());
-        })
-        return ((startsWithDark !== undefined) || (endsWithDark !== undefined))
+        result.route = token.$schema.route // Add route from parser
+        result.mode = parseMode({$schema: result})
+        Object.assign(token.$schema, result)
     }
 }
 
@@ -123,35 +33,6 @@ const systemSchemas = () => {
     return result
 }
 
-const assignSchema = (token, schemas) => {
-
-    if (!schemas) schemas = systemSchemas()
-
-    const route = routeCleaner(token.$schema.route)
-    const schema = schemas.filter(schema => !token.$schema.mapped && route.endsWith(schema.key))
-
-    if (schema.length === 0) return undefined
-
-    if (schema.length > 1) throw new Error(`"${schema}" has more than one value for tokenAttributesForKey. 
-    All taxonomy definitions need to be unique.`);
-
-    let result = schema[0]
-    if (result) {
-        result.route = token.$schema.route // Add route back in
-        result.mapped = true // Set mapped to true, so it is not remapped later.
-        result.mode = parseMode({$schema: result})
-        Object.assign(token.$schema, result)
-    }
-
-}
-
 module.exports = {
-    setSchemaForToken,
-    assignSchema,
-    setModeForToken,
-    FANG_SCHEMAS: [
-        ...FANG_PALETTE, 
-        ...FANG_CONTEXTUAL 
-    ],
-    fang_size: FANG_SIZE,
+    assignSchema
 }
